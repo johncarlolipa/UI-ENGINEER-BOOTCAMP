b44236af4caf477b1eab645a1ad29fa7ca1598df
JC Lipa <your_email@example.com>
2025-11-30 10:45:57 +0800

    tweak button component

--- ui-engineer-lessons/app/lesson-one/Button.tsx
+++ ui-engineer-lessons/app/lesson-one/Button.tsx
@@ -1,63 +1,58 @@
-import { ReactNode } from "react";
-import clsx from "clsx";
 
+import clsx from "clsx";
+import React, { ReactNode} from "react";
+
+
+//define the variants
 type ButtonSize = "small" | "medium" | "large";
-type ButtonVariant = "filled" | "outline";
-type ButtonType = "primary" | "error" | "warning" | "success" | "info" | "disabled";
+type ButtonVariants = "filled" | "outline";
+type ButtonType = "primary" | "error" | "success" | "info" | "warning" | "disabled";
 
 type ButtonProps = {
   text: string;
-  iconLeft?: ReactNode;
-  iconRight?: ReactNode;
+  arrowLeft?: ReactNode;
+  arrowRight?: ReactNode;
   size?: ButtonSize;
-  variant?: ButtonVariant;
+  variant?: ButtonVariants;
   type?: ButtonType;
-};
+}
 
-export function Button({
-  text,
-  iconLeft,
-  iconRight,
-  size = "medium",
-  variant = "filled",
-  type = "primary",
-}: ButtonProps) {
+export function Button ({
+  text, size = "medium", variant = "filled", arrowLeft, arrowRight, type= "primary"}:ButtonProps
+){
   const baseClasses = "rounded-md flex items-center gap-2 transition-colors duration-300";
 
   const sizeClasses = {
     small: "text-sm py-1 px-2",
     medium: "text-base py-2 px-4",
-    large: "text-lg py-3 px-6",
+    large: "text-lg py-3 px-6"
   };
 
   const variantClasses = {
     filled: {
-      primary: "bg-blue-500 text-white hover:bg-blue-600 focus:bg-blue-700 focus:ring-2 focus:ring-blue-800 focus:outline-none",
-      error: "bg-red-500 text-white hover:bg-red-600 focus:ring-2 focus:ring-red-800 focus:outline-none",
-      success: "bg-green-500 text-white hover:bg-green-600 focus:bg-green-700 focus:ring-2 focus:ring-green-800 focus:outline-none",
-      warning: "bg-yellow-500 text-white hover:bg-yellow-600 focus:bg-yellow-700 focus:ring-2 focus:ring-yellow-800 focus:outline-none",
-      info: "bg-indigo-500 text-white hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-800 focus:outline-none",
-      disabled: "bg-gray-400 text-white cursor-not-allowed",
-    },
-    outline: {
-      primary: "border border-blue-500 text-blue-500 hover:bg-blue-400 hover:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none",
-      error: "border border-red-500 text-red-500 hover:bg-red-400 hover:text-white focus:ring-2 focus:ring-red-500 focus:outline-none",
-      success: "border border-green-500 text-green-500 hover:bg-green-400 hover:text-white focus:ring-2 focus:ring-green-500 focus:outline-none",
-      warning: "border border-yellow-500 text-yellow-500 hover:bg-yellow-400 hover:text-white focus:ring-2 focus:ring-yellow-500 focus:outline-none",
-      info: "border border-indigo-500 text-indigo-500 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-500 focus:outline-none",
-      disabled: "border border-gray-400 text-gray-400 cursor-not-allowed",
-    },
-  
-  };
+            primary: "bg-blue-500 text-white hover:bg-blue-600 focus:bg-blue-700 focus:ring-2 focus:ring-blue-800 focus:outline-none",
+            error: "bg-red-500 text-white hover:bg-red-600 focus:ring-2 focus:ring-red-800 focus:outline-none",
+            success: "bg-green-500 text-white hover:bg-green-600 focus:bg-green-700 focus:ring-2 focus:ring-green-800 focus:outline-none",
+            warning: "bg-yellow-500 text-white hover:bg-yellow-600 focus:bg-yellow-700 focus:ring-2 focus:ring-yellow-800 focus:outline-none",
+            info: "bg-indigo-500 text-white hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-800 focus:outline-none",
+            disabled: "bg-gray-400 text-white cursor-not-allowed",
+          },
+          outline: {
+            primary: "border border-blue-500 text-blue-500 hover:bg-blue-400 hover:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none",
+            error: "border border-red-500 text-red-500 hover:bg-red-400 hover:text-white focus:ring-2 focus:ring-red-500 focus:outline-none",
+            success: "border border-green-500 text-green-500 hover:bg-green-400 hover:text-white focus:ring-2 focus:ring-green-500 focus:outline-none",
+            warning: "border border-yellow-500 text-yellow-500 hover:bg-yellow-400 hover:text-white focus:ring-2 focus:ring-yellow-500 focus:outline-none",
+            info: "border border-indigo-500 text-indigo-500 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-500 focus:outline-none",
+            disabled: "border border-gray-400 text-gray-400 cursor-not-allowed",
+          },
+  }
 
-  return (
-    <button
-      className={clsx(baseClasses, sizeClasses[size], variantClasses[variant][type])}
-      disabled={type === "disabled"}
-    >
-      {iconLeft && <span>{iconLeft}</span>}
-      <span>{text}</span>
-      {iconRight && <span>{iconRight}</span>}
-    </button>
-  );
+return (
+  <button className={clsx(baseClasses, sizeClasses[size], variantClasses[variant][type])} disabled={type === "disabled"}>
+    {arrowLeft && <span>{arrowLeft}</span>}
+  <span>{text}</span>
+  {arrowRight && <span>{arrowRight}</span>}
+  </button>
+)
+
 }
--- ui-engineer-lessons/app/page.tsx
+++ ui-engineer-lessons/app/page.tsx
@@ -4,17 +4,18 @@ import { ArrowLeft, ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-zinc-50 font-sans dark:bg-white text-black">
-      <Button text="Primary Filled" type="primary" variant="filled" iconLeft={<ArrowLeft size={16} />} iconRight={<ArrowRight size={16} />} />
-      <Button text="Error Filled" type="error" variant="filled" iconLeft={<ArrowLeft size={16} />} iconRight={<ArrowRight size={16} />} />
-      <Button text="Success Outline" type="success" variant="outline" iconLeft={<ArrowLeft size={16} />} iconRight={<ArrowRight size={16} />} />
-      <Button text="Disabled Filled" type="disabled" variant="filled" iconLeft={<ArrowLeft size={16} />} iconRight={<ArrowRight size={16} />} />
-      <Button text="Warning Filled" type="warning" variant="filled" iconLeft={<ArrowLeft size={16} />} iconRight={<ArrowRight size={16} />} />
-      <Button text="Info Outline" type="info" variant="outline" iconLeft={<ArrowLeft size={16} />} iconRight={<ArrowRight size={16} />} />
-      <Button text="Large Filled" type="success" variant="filled" size="large" />
-      <Button text="Small Outline" type="error" variant="outline" size="small" />
-      <Button text="Medium Outline" type="disabled" variant="outline" size="medium" />
-      <Button text="Medium Warning Outline" type="warning" variant="outline" size="medium" />
-      <Button text="Large Info Filled" type="info" variant="filled" size="large" />
+      <Button text="Primary" type="primary" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Success" type="success" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Error" type="error" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Info" type="info" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Disabled" type="disabled" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+
+
+      <Button text="Primary" type="primary" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Success" type="success" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Error" type="error" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Info" type="info" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Disabled" type="disabled" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
     </div>
   );
 }
