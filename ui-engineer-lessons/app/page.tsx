7f64d2d6d0072718a0d2e47b907ae0bedcee9b7d
JC Lipa <your_email@example.com>
2025-12-05 07:56:18 +0800

    create card component

--- ui-engineer-lessons/app/page.tsx
+++ ui-engineer-lessons/app/page.tsx
@@ -1,20 +1,28 @@
 import { Button } from "./lesson-one/Button";
+import { Card } from "./lesson-one/Card";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-zinc-50 font-sans dark:bg-white text-black">
       <Button text="Primary" type="primary" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text='Success' type='success' variant ="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}  />
-      <Button text="Error" type="error" variant = "filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Warning" type="warning" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}/>
-      <Button text="Information" type="info" variant = "filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} /> 
+      <Button text='Success' type='success' variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Error" type="error" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Warning" type="warning" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Information" type="info" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
 
       <Button text="Primary" type="primary" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text='Success' type='success' variant ="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}  />
-      <Button text="Error" type="error" variant = "outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Warning" type="warning" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}/>
-      <Button text="Information" type="info" variant = "outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} /> 
-    </div> 
+      <Button text='Success' type='success' variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Error" type="error" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Warning" type="warning" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Information" type="info" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+
+
+      <Card size="large" variant="shadow" imageSrc="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=tinysrgb&fit=max&w=800" imageAlt="beautiful beach" title="Discover New Ideas" subtitle="Inspiration & Creativity" description="Explore stories, learn techniques, and find inspiration every day." tags={["Design", "UX", "Inspire"]} footer={
+        <div className="flex justify-end gap-2"><Button text="Save" variant="outline" type="info" /><Button text="Open" variant="filled" type="primary" /></div>}
+      />
+
+
+    </div>
   );
 }
