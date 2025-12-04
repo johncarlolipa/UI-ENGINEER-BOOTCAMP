6e30220d362c3ecb809caa92cfb1901458eaf30c
JC Lipa <your_email@example.com>
2025-12-05 05:29:19 +0800

    rewrite button

--- ui-engineer-lessons/app/lesson-one/Button.tsx
+++ ui-engineer-lessons/app/lesson-one/Button.tsx
@@ -1,12 +1,72 @@
 
-import clsx from "clsx";
-import React, { ReactNode} from "react";
+// import clsx from "clsx";
+// import React, { ReactNode} from "react";
 
 
-//define the variants
-type ButtonSize = "small" | "medium" | "large";
-type ButtonVariants = "filled" | "outline";
-type ButtonType = "primary" | "error" | "success" | "info" | "warning" | "disabled";
+// //define the variants
+// type ButtonSize = "small" | "medium" | "large";
+// type ButtonVariants = "filled" | "outline";
+// type ButtonType = "primary" | "error" | "success" | "info" | "warning" | "disabled";
+
+// //define the component props
+// type ButtonProps = {
+//   text: string;
+//   arrowLeft?: ReactNode;
+//   arrowRight?: ReactNode;
+//   size?: ButtonSize;
+//   variant?: ButtonVariants;
+//   type?: ButtonType;
+// }
+
+// //state the function
+// export function Button ({
+//   text, size = "medium", variant = "filled", arrowLeft, arrowRight, type= "primary"}:ButtonProps
+// ){
+//   const baseClasses = "rounded-md flex items-center gap-2 transition-colors duration-300";
+
+//   const sizeClasses = {
+//     small: "text-sm py-1 px-2",
+//     medium: "text-base py-2 px-4",
+//     large: "text-lg py-3 px-6"
+//   };
+
+//   const variantClasses = {
+//     filled: {
+//             primary: "bg-blue-500 text-white hover:bg-blue-600 focus:bg-blue-700 focus:ring-2 focus:ring-blue-800 focus:outline-none",
+//             error: "bg-red-500 text-white hover:bg-red-600 focus:ring-2 focus:ring-red-800 focus:outline-none",
+//             success: "bg-green-500 text-white hover:bg-green-600 focus:bg-green-700 focus:ring-2 focus:ring-green-800 focus:outline-none",
+//             warning: "bg-yellow-500 text-white hover:bg-yellow-600 focus:bg-yellow-700 focus:ring-2 focus:ring-yellow-800 focus:outline-none",
+//             info: "bg-indigo-500 text-white hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-800 focus:outline-none",
+//             disabled: "bg-gray-400 text-white cursor-not-allowed",
+//           },
+//     outline: {
+//             primary: "border border-blue-500 text-blue-500 hover:bg-blue-400 hover:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none",
+//             error: "border border-red-500 text-red-500 hover:bg-red-400 hover:text-white focus:ring-2 focus:ring-red-500 focus:outline-none",
+//             success: "border border-green-500 text-green-500 hover:bg-green-400 hover:text-white focus:ring-2 focus:ring-green-500 focus:outline-none",
+//             warning: "border border-yellow-500 text-yellow-500 hover:bg-yellow-400 hover:text-white focus:ring-2 focus:ring-yellow-500 focus:outline-none",
+//             info: "border border-indigo-500 text-indigo-500 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-500 focus:outline-none",
+//             disabled: "border border-gray-400 text-gray-400 cursor-not-allowed",
+//           },
+//   }
+
+
+// return (
+//   <button className={clsx(baseClasses, sizeClasses[size], variantClasses[variant][type])} disabled={type === "disabled"}>
+//     {arrowLeft && <span>{arrowLeft}</span>}
+//   <span>{text}</span>
+//   {arrowRight && <span>{arrowRight}</span>}
+//   </button>
+// )
+
+// }
+
+
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+type ButtonSize = 'small' | 'medium' | "large";
+type ButtonVariants = 'filled' | 'outline';
+type ButtonType = "primary" | "error" | "success" | 'info' | "warning" | "disabled"
 
 type ButtonProps = {
   text: string;
@@ -17,42 +77,44 @@ type ButtonProps = {
   type?: ButtonType;
 }
 
-export function Button ({
-  text, size = "medium", variant = "filled", arrowLeft, arrowRight, type= "primary"}:ButtonProps
-){
-  const baseClasses = "rounded-md flex items-center gap-2 transition-colors duration-300";
+
+export function Button({
+  text, size = 'medium', variant = "filled", type = "primary", arrowLeft, arrowRight
+}: ButtonProps) {
+  const baseClasses = 'rounded-md flex items-center gap-2 transition-colors duration-300';
 
   const sizeClasses = {
     small: "text-sm py-1 px-2",
     medium: "text-base py-2 px-4",
     large: "text-lg py-3 px-6"
-  };
+  }
 
   const variantClasses = {
     filled: {
-            primary: "bg-blue-500 text-white hover:bg-blue-600 focus:bg-blue-700 focus:ring-2 focus:ring-blue-800 focus:outline-none",
-            error: "bg-red-500 text-white hover:bg-red-600 focus:ring-2 focus:ring-red-800 focus:outline-none",
-            success: "bg-green-500 text-white hover:bg-green-600 focus:bg-green-700 focus:ring-2 focus:ring-green-800 focus:outline-none",
-            warning: "bg-yellow-500 text-white hover:bg-yellow-600 focus:bg-yellow-700 focus:ring-2 focus:ring-yellow-800 focus:outline-none",
-            info: "bg-indigo-500 text-white hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-800 focus:outline-none",
-            disabled: "bg-gray-400 text-white cursor-not-allowed",
-          },
-          outline: {
-            primary: "border border-blue-500 text-blue-500 hover:bg-blue-400 hover:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none",
-            error: "border border-red-500 text-red-500 hover:bg-red-400 hover:text-white focus:ring-2 focus:ring-red-500 focus:outline-none",
-            success: "border border-green-500 text-green-500 hover:bg-green-400 hover:text-white focus:ring-2 focus:ring-green-500 focus:outline-none",
-            warning: "border border-yellow-500 text-yellow-500 hover:bg-yellow-400 hover:text-white focus:ring-2 focus:ring-yellow-500 focus:outline-none",
-            info: "border border-indigo-500 text-indigo-500 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-500 focus:outline-none",
-            disabled: "border border-gray-400 text-gray-400 cursor-not-allowed",
-          },
+      primary: "bg-blue-500 text-white hover:bg-blue-600 focus:bg-blue-700 focus:ring-2 focus:ring-blue-800 focus:outline-none",
+      error: "bg-red-500 text-white hover:bg-red-600 focus:ring-2 focus:ring-red-800 focus:outline-none",
+      success: "bg-green-500 text-white hover:bg-green-600 focus:bg-green-700 focus:ring-2 focus:ring-green-800 focus:outline-none",
+      warning: "bg-yellow-500 text-white hover:bg-yellow-600 focus:bg-yellow-700 focus:ring-2 focus:ring-yellow-800 focus:outline-none",
+      info: "bg-indigo-500 text-white hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-800 focus:outline-none",
+      disabled: "bg-gray-400 text-white cursor-not-allowed",
+    },
+    outline: {
+      primary: "border border-blue-500 text-blue-500 hover:bg-blue-400 hover:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none",
+      error: "border border-red-500 text-red-500 hover:bg-red-400 hover:text-white focus:ring-2 focus:ring-red-500 focus:outline-none",
+      success: "border border-green-500 text-green-500 hover:bg-green-400 hover:text-white focus:ring-2 focus:ring-green-500 focus:outline-none",
+      warning: "border border-yellow-500 text-yellow-500 hover:bg-yellow-400 hover:text-white focus:ring-2 focus:ring-yellow-500 focus:outline-none",
+      info: "border border-indigo-500 text-indigo-500 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-500 focus:outline-none",
+      disabled: "border border-gray-400 text-gray-400 cursor-not-allowed",
+    }
   }
 
-return (
-  <button className={clsx(baseClasses, sizeClasses[size], variantClasses[variant][type])} disabled={type === "disabled"}>
-    {arrowLeft && <span>{arrowLeft}</span>}
-  <span>{text}</span>
-  {arrowRight && <span>{arrowRight}</span>}
-  </button>
-)
 
+  return (
+    <button className={clsx(baseClasses, sizeClasses[size], variantClasses[variant][type])} disabled={type === 'disabled'}>
+      {arrowLeft && <span>{arrowLeft}</span>}
+      <span>{text}</span>
+      {arrowRight && <span>{arrowRight}</span>}
+    </button>
+  )
 }
+
--- ui-engineer-lessons/app/page.tsx
+++ ui-engineer-lessons/app/page.tsx
@@ -5,17 +5,16 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-zinc-50 font-sans dark:bg-white text-black">
       <Button text="Primary" type="primary" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Success" type="success" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Error" type="error" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Info" type="info" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Disabled" type="disabled" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-
+      <Button text='Success' type='success' variant ="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}  />
+      <Button text="Error" type="error" variant = "filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Warning" type="warning" variant="filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}/>
+      <Button text="Information" type="info" variant = "filled" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} /> 
 
       <Button text="Primary" type="primary" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Success" type="success" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Error" type="error" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Info" type="info" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-      <Button text="Disabled" type="disabled" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
-    </div>
+      <Button text='Success' type='success' variant ="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}  />
+      <Button text="Error" type="error" variant = "outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} />
+      <Button text="Warning" type="warning" variant="outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />}/>
+      <Button text="Information" type="info" variant = "outline" arrowLeft={<ArrowLeft size={16} />} arrowRight={<ArrowRight size={16} />} /> 
+    </div> 
   );
 }
